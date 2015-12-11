6da4aa87bcfa19aa90b5f1ffaa389a41b160331b
ksliker89 <kevin.sliker@gmail.com>
2015-12-11 10:28:07 -0800

    assignment4

---
+++ assignment4/compare2.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <assert.h>
+#include "bst.h"
+#include "structs.h"
+
+/*----------------------------------------------------------------------------
+  very similar to the compareTo method in java or the strcmp function in c. it
+  returns an integer to tell you if the left cost is greater then, less then, or
+  equal to the right cost. you are comparing the number variable, letter is not
+  used in the comparison.
+
+  if left < right return -1
+  if left > right return 1
+  if left = right return 0
+ */
+
+/*Define this function, type casting the cost of void * to the desired type.
+  The current definition of TYPE in bst.h is void*, which means that left and
+  right are void pointers. To compare left and right, you should first cast
+  left and right to the corresponding pointer type (struct pizza *), and then
+  compare the costs pointed by the casted pointers.
+
+  DO NOT compare the addresses pointed by left and right, i.e. "if (left < right)",
+  which is really wrong.
+ */
+int compare(TYPE left, TYPE right)
+{
+	//first cast the left and right from (void *) to (struct pizza *)
+	struct pizza * port = (struct pizza *)left;
+	struct pizza * starboard = (struct pizza *)right;
+
+	//now make the comparisons
+	if(port->cost < starboard->cost)
+	{
+		return -1;
+	}
+
+	if(port->cost > starboard->cost)
+	{
+		return 1;
+	}
+	
+	return 0;
+}
+
+/*Define this function, type casting the cost of void * to the desired type*/
+void print_type(TYPE curval)
+{
+	struct pizza * current = (struct pizza *)curval;
+	printf("%f\n", current->cost);
+}
---
+++ assignment4/structs.h
@@ -0,0 +1,16 @@
+/* You can modify the structure to store whatever you'd like in your BST */
+
+struct data 
+{
+	int number;
+	char *name;
+};
+
+/*
+struct pizza 
+{
+	double cost;
+	int toppings;
+	char *name;
+};
+*/
